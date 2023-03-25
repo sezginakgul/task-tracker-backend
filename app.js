147d0b1fbdef6be16da098ae754e2f5820b34ad4
sezgin <sezgin.akgull@gmail.com>
2023-03-25 23:08:09 +0300

    added database connection code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+require("./src/config/databaseConnection");
 const port = process.env.PORT || 5001;
 const todoRouter = require("./src/routers/todoRouter");
 
---
+++ src/config/databaseConnection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Veritabanına Başarıyla Bağlandı.");
+  })
+  .catch((err) => {
+    console.log("Veritabanına Bağlanılmadı : " + err);
+  });
